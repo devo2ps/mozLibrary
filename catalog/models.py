9ef578809ac9597c035f5f3e10f6284097e8b8b7
devo2ps <jjbrandenbur@wisc.edu>
2019-10-31 19:28:14 -0700

    added author list and detail view

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 import uuid
-
+from django.contrib.auth.models import User
+from datetime import date
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -51,6 +52,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+    	if self.due_back and date.today() > self.due_back:
+        	return True
+    	return False
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -66,13 +74,18 @@ class BookInstance(models.Model):
         default='m',
         help_text='Book availability',
     )
+    
+  
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)   
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+
 
 
 class Author(models.Model):
